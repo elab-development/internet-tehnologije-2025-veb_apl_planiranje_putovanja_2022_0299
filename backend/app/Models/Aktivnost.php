249e38a984bcef3ea39a2c1169bccb4e574f4b63
teaaaa3333 <janicijevic.t03@gmail.com>
2026-01-26 19:08:08 +0100

    Implement user authentication (register, login, logout)

--- backend/app/Models/Aktivnost.php
+++ backend/app/Models/Aktivnost.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Aktivnost extends Model
 {
     use HasFactory;    
 
     protected $fillable = [
+        'destinacija_id',
         'ime',
         'naziv',
         'cena',
