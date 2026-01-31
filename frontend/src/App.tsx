f3e1ca979f5552ac4f9a5867f8e8dc12c6ea6600
dperiic <danica03peric@gmail.com>
2026-01-31 19:04:30 +0100

    Navbar & MenuBar

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/logo2.svg'
+import viteLogo from '/logo.png'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Hotel from './pages/Hotel';
+import Restaurant from './pages/Restaurant';
+import Navbar from './components/Navbar';
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/hotels/:id' element={<Hotel />} />
+        <Route path='/restaurants/:id' element={<Restaurant />} />
+      </Routes>
+    </BrowserRouter>
+  );
+  
+}
+
+export default App
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import MenuBar from '../components/home/MenuBar';
+
+const Home = () => {
+  return (
+    <div>
+      <MenuBar />
+    </div>
+  );
+  };
+export default Home;
